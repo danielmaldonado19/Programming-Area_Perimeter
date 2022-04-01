8c2f64cac91a44647070e8320ca5a3d4af9155b3
Daniel Maldonado <damaldy19@hotmail.com>
2022-03-31 19:01:27 -0500

    number of decimals reduced to 2

--- figures.js
+++ figures.js
@@ -5,6 +5,8 @@
 4.Input's configuration which will point out how many sides has the polygon the program will execute.
 5.Configuration of the button which will run the program.
 6.Function "calculate()" created for the program running once the user click the button.
+7.Display the calculations's results in the site.
+8.Configure the area and perimeter values to only display 2 decimals.
 */
 
 //1.-----------------------------------------------------------
@@ -29,17 +31,26 @@ function calculate(){
         var apothem = sideLength / (2 * Math.tan(theta * Math.PI / 180));
         area = (perimeter * apothem) / 2;
 
+        perimeter = perimeter.toFixed(2);
+        area = area.toFixed(2);
+        
+
+
     }
     else if(sidesNum == 1){
         var radius = window.prompt("Whta's the circle's radius");
         var perimeter = 2 * Math.PI * radius;
         var area = Math.PI * radius * radius;
 
+        perimeter = perimeter.toFixed(2);
+        area = area.toFixed(2);
+
     }
     //3.------------------------------------------------
     else{
         console.log("Invalid value entered. Try again");
     }
+    //7.---------------------------------------------------------------------------------------------------------------------
     console.log(perimeter);
     console.log(area);
     result.innerHTML = "Your polygon has " + sidesNum + " sides. Its perimeter is " + perimeter + " and its area is " + area;
