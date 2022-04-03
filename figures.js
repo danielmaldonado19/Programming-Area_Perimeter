2555f6d378a2d500a2e8f74a19164ae488657df7
Daniel Maldonado <damaldy19@hotmail.com>
2022-04-03 15:55:59 -0500

    Code cleaned, only functions stay

--- figures.js
+++ figures.js
@@ -10,16 +10,7 @@ function cirArea(radius){
     return circleArea;
 }; 
 
-/*
-var circlePerimeter = circlePerimeter.toFixed(2);
-var circleArea = circleArea.toFixed(2);
 
-console.group("Circle's data");
-console.log("The radius of the circle's: " + circleRadius + " cm");
-console.log("The circle's perimeter's: " + circlePerimeter + " cm");
-console.log("The circle's area's: " + circleArea + " cm^2");
-console.groupEnd();
-*/
 
 //Triangle
 
@@ -39,18 +30,7 @@ function triaArea(sidesLength){
     return triangleArea;
 }
 
-/*
-var triangleHeight = triangleHeight.toFixed(2);
-var trianglePerimeter = trianglePerimeter.toFixed(2);
-var triangleArea = triangleArea.toFixed(2);
 
-
-console.group("Triangle's data");
-console.log("The triangle's height's: " + triangleHeight + " cm");
-console.log("The triangle's perimeter's: " + trianglePerimeter + " cm");
-console.log("The triangle's area's: " + triangleArea + " cm^2");
-console.groupEnd();
-*/
 
 //Square
 
@@ -64,12 +44,3 @@ function squaArea(sidesLength){
     return squareArea;
 }
 
-/*
-var squarePerimeter = squarePerimeter.toFixed(2);
-var squareArea = squareArea.toFixed(2);
-
-console.group("Square's data");
-console.log("The square's perimeter's: " + squarePerimeter + " cm");
-console.log("The square's area's: " + squareArea + " cm^2");
-console.groupEnd();
-*/
